06e1c158680e629b5b5d3d068dd464d2c4516e43
mark_chiong@rcc.edu.ph <MRChiong>
2024-05-15 15:54:03 +0800

    first

---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AIMS</title>
+
+</head>
+
+    <style>
+        body {background-color: #F7DCB9}
+        h1 {text-align: center;
+            font-family: Verdana
+        }
+        p {text-align: center;
+           font-family: Verdana
+        }
+        li {text-align: center;
+            font-family: Verdana
+        }
+        img {display: block;
+             margin-left: auto;
+             margin-right: auto;
+             border:1px solid black
+            }
+        
+    </style>
+
+    <body>
+    <img src="home.gif" alt="home icon" width="150" height="150" class ="center">
+    <h1>Welcome to Academic Information Management System</h1>
+    <p>Please select an option:</p>
+
+        <li><a href="input_form.php">Input New Record</a></li>
+        <li><a href="delete.php">Delete Record</a></li>
+    
+    </body>
+</html>
